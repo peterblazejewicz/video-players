c927f5bd7e939fd3839ed15121d6a090d5879995
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-12-18 21:48:40 +0100

    Basic public properties of JsonpLibrary

--- libs/jsonp-library/src/lib/jsonp-library/jsonp-library.component.spec.ts
+++ libs/jsonp-library/src/lib/jsonp-library/jsonp-library.component.spec.ts
@@ -3,22 +3,39 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { JsonpLibraryComponent } from './jsonp-library.component';
 
 describe('JsonpLibraryComponent', () => {
-  let component: JsonpLibraryComponent;
-  let fixture: ComponentFixture<JsonpLibraryComponent>;
+  describe('Basic properties', () => {
+    let component: JsonpLibraryComponent;
+    let fixture: ComponentFixture<JsonpLibraryComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [JsonpLibraryComponent]
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [JsonpLibraryComponent]
+      }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(JsonpLibraryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+      fixture = TestBed.createComponent(JsonpLibraryComponent);
+      component = fixture.componentInstance;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should have libraryUrl property with undefined default value', () => {
+      expect(component.libraryUrl).toBeUndefined();
+    });
+
+    it('shuld have callbackName property with undefined default value', () => {
+      expect(component.callbackName).toBeUndefined();
+    });
+
+    it('shuld have notifyEventName property with undefined default value', () => {
+      expect(component.notifyEventName).toBeUndefined();
+    });
+
+    it('shuld have notifyEvent property with default value', () => {
+      expect(component.notifyEvent).toBeDefined();
+    });
   });
 });
--- libs/jsonp-library/src/lib/jsonp-library/jsonp-library.component.ts
+++ libs/jsonp-library/src/lib/jsonp-library/jsonp-library.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -9,7 +9,64 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class JsonpLibraryComponent implements OnInit {
-  constructor() {}
+  private _libraryUrl: string;
 
-  ngOnInit() {}
+  private ready = false;
+
+  /**
+   * Library url. Must contain string `%%callback%%`.
+   * `%%callback%%` is a placeholder for jsonp wrapper function name
+   * Ex: https://maps.googleapis.com/maps/api/js?callback=%%callback%%
+   * @type {string}
+   * @memberof JsonpLibraryComponent
+   */
+  @Input()
+  set libraryUrl(libraryUrl: string) {
+    this._libraryUrl = libraryUrl;
+    if (this.ready && libraryUrl) {
+      this.loadLibrary();
+    }
+  }
+
+  get libraryUrl(): string {
+    return this._libraryUrl;
+  }
+
+  /**
+   * Set if library requires specific callback name.
+   * Name will be automatically generated if not set.
+   * @type {string}
+   * @memberof JsonpLibraryComponent
+   */
+  @Input()
+  callbackName: string;
+
+  /**
+   * name of event to be emitted when library loads. Standard is `api-load`
+   * @type {string}
+   * @memberof JsonpLibraryComponent
+   */
+  @Input()
+  notifyEventName: string;
+
+  /**
+   * event with name specified in `notifyEvent` attribute
+   * will fire upon successful load
+   * @type {EventEmitter<any>}
+   * @memberof JsonpLibraryComponent
+   */
+  @Output()
+  notifyEvent: EventEmitter<any> = new EventEmitter();
+  ngOnInit() {
+    this.ready = true;
+    if (this.libraryUrl) {
+      this.loadLibrary();
+    }
+  }
+
+  /**
+   * @private
+   * @memberof JsonpLibraryComponent
+   */
+  private loadLibrary() {}
 }
